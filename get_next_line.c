b3da91ed60fa259764a74aeebb3d4ec19f6c78f7
Ri-Hani HENDRIKS <rhendrik@c4r19s2.wethinkcode.co.za>
2019-06-24 16:08:47 +0200

    hope this works

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,70 @@
 /*   By: rhendrik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 08:11:07 by rhendrik          #+#    #+#             */
-/*   Updated: 2019/06/21 10:02:21 by rhendrik         ###   ########.fr       */
+/*   Updated: 2019/06/24 16:03:01 by rhendrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
 
-int getlen(const int fd, char **line)
+int get_next_place(char **s, char **line)
 {
-	char *s;
-	int len;
+	char *tmp;
+	size_t len;
 
-	s = line[fd];
 	len = 0;
-	while (*s != '\n' || *s != EOF)
-	{
-		s++;
+	while ((*s)[len] != '\n' || (*s)[len] != '\0')
 		len++;
+	if ((*s)[len] == '\n')
+	{
+		*line = ft_strsub(*s, 0, len);
+		tmp = ft_strdup(&*s[len + 1]);
+		free(*s);
+		*s = tmp;
+		if ((*s)[0] == '\0')
+			ft_strdel(s);
 	}
-	return (len);
+	else
+	{
+		*line = ft_strdup(*s);
+		ft_strdel(s);
+	}
+	return (1);
+}
+
+int output(char **s, char **line, int ret, int fd)
+{
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && s[fd] ==NULL)
+		return (0);
+	else
+		return get_next_place(&s[fd], line);
 }
 
 int get_next_line(const int fd, char **line)
 {
+	int ret;
+	static char **s;
+	char buff[BUFF_SIZE + 1];
+	char *tmp;
+
 	if (fd < 0 || line == NULL)
 		return (-1);
-	return (0);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		if (s[fd] == NULL)
+			s[fd] = ft_strdup(buff);
+		else
+		{
+			tmp = ft_strjoin(s[fd], buff);
+			free(s[fd]);
+			s[fd] = tmp;
+		}
+		if (ft_strchr(s[fd], '\n'))
+			break ;
+	}
+	return (output(s, line, ret, fd));
 }
