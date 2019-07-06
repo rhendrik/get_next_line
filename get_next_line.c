aa4e20bb4351f730c0de81794c5ac8a28f448519
Ri-Hani HENDRIKS <rhendrik@c4r19s2.wethinkcode.co.za>
2019-07-06 16:44:00 +0200

    It's working! Thank you Marnus

--- get_next_line.c
+++ get_next_line.c
@@ -5,69 +5,82 @@
  ** 1, 0 or -1, depending on whether or not the 
     read has been sucessful */
 
-int has_read(char *s[255], int fd, char *buff)
+int has_read(char **s, int fd, char *buff)
 {
 	int hasread;
+	char *tmp;
 
-	if ((hasread = read(fd, buff, BUFF_SIZE)) == 0)
-		return (0);
-	if ((hasread = read(fd, buff, BUFF_SIZE)) < 0)
+	if ((hasread = read(fd, buff, 0)) < 0)
 		return (-1);
+	//if ((hasread = read(fd, buff, BUFF_SIZE)) == 0)
+	//	return (0);
 	while ((hasread = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[hasread] = '\0';
-		*s = ft_strjoin(*s, buff);
+		//temp = *s;
+		tmp = ft_strjoin(*s, buff);
+		ft_strdel(s);
+		*s = ft_strdup(tmp);
+		ft_strdel(&tmp);
 	}
+	if (!buff[0] && !(*s)[0])
+		return (0);
 	return (1);
 }
 
-int get_len(char *s)
+int get_len(char **s)
 {
 	int len;
 	len = 0;
 
-	while (s[len] != '\n' || s[len] != '\0')
+	while ((*s)[len] != '\n' &&	(*s)[len] != '\0')
 		len++;
 	return (len);
 }
 
-int set_str(char *s[255], char **line, char *tmp, int fd)
+int set_str(char **s, char **line, char *tmp)
 {
 	int len;
+	char	*tmp2;
 
-	len = get_len(s[fd]);
-	if((tmp = ft_strchr(s[fd], '\n')) == NULL)
+	if((tmp = ft_strchr(*s, '\n')) == NULL && *s[0] != '\0')
 	{
-		*line = ft_strdup(s[fd]);
+		*line = ft_strdup(*s);
+		ft_strdel(s);
+	}
+	else if (*s[0] == '\0')
+	{
 		return (0);
 	}
 	else
 	{
-		tmp = ft_strchr(s[fd], '\n') + 1;
-		*line = ft_strsub(s[fd], 0, len);
-		free(s[fd]);
-		s[fd] = tmp;
-		free(tmp);
-		return (1);
+		len = get_len(s);
+		*tmp = '\0';
+		*line = ft_strdup(*s);
+	
+		tmp2 = ft_strdup(tmp +1);
+		//tmp = ft_strsub(*s, len, len - ft_strlen(tmp));
+		ft_strdel(s);
+		*s = ft_strdup(tmp2);
+		ft_strdel(&tmp2);
 	}
-	return (-1);
+	return (1);
 }
 
 int get_next_line(const int fd, char **line)
 {
-	char *buff;
-	static char *s[255];
+	char buff[BUFF_SIZE + 1];
+	static char *s[1024];
 	char *tmp;
 
 	tmp = NULL;
 	if (!s[fd])
-		s[fd] = ft_strnew(0);
-	buff = (char *)malloc(BUFF_SIZE + 1 * sizeof(char));
-	if (line == NULL || fd < 0 || read(fd, buff, BUFF_SIZE) < 0)
+		s[fd] = ft_strnew(1);
+	buff[0] = '\0';
+	if (line == NULL || fd < 0 || read(fd, buff, 0) < 0)
 		return (-1);
-	if (has_read(s, fd, buff) < 0)
+	if (has_read(&(s[fd]), fd, buff) < 0)
 		return (-1);
-	buff[fd] = '\0';
-	return (set_str(s, line, tmp, fd));
+	return (set_str(&(s[fd]), line, tmp));
 }
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rhendrik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 18:07:16 by rhendrik          #+#    #+#             */
-/*   Updated: 2019/06/27 13:23:15 by rhendrik         ###   ########.fr       */
+/*   Updated: 2019/07/06 15:31:44 by rhendrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,21 @@
 int main(void)
 {
 	int fd;
-	int gnl;
 	char *line;
+	int i;
 
+	i = 0;
 	line = NULL;
-	fd = open("author", O_RDONLY);
-	gnl = get_next_line(fd, &line);
-	if (gnl == 1 || gnl == 1)
-		ft_putstr("Yaay!\n");
-	ft_putstr(line);
+	fd = open("tester", O_RDONLY);
+	while(get_next_line(fd, &line) > 0)
+	{
+		ft_putstr(line);
+		ft_putchar('\n');
+		ft_strdel(&line);
+		i++;
+	}
+	ft_putstr("Yaay!\n");
 	ft_putchar('\n');
+	sleep (100);
 	return (0);
 }
