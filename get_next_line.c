160d0f0718dfc583b3d7ebceaf0f7ff02b3529eb
rhendrik <rhendrik@student.wethinkcode.co.za>
2019-08-19 14:01:05 +0200

    working on it

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rhendrik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 16:45:59 by rhendrik          #+#    #+#             */
-/*   Updated: 2019/08/06 10:33:44 by rhendrik         ###   ########.fr       */
+/*   Updated: 2019/08/19 12:34:24 by rhendrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,24 @@ static int	has_read(char **s, int fd, char *buff)
 	int		hasread;
 	char	*tmp;
 
-	if ((hasread = read(fd, buff, 0)) < 0)
-		return (-1);
-	while ((hasread = read(fd, buff, BUFF_SIZE)) > 0)
+	while (!(ft_strchr(buff, '\n')))
 	{
+		hasread = read(fd, buff, BUFF_SIZE);
+		if (hasread < 0)
+			return (-1);
+	}
 		buff[hasread] = '\0';
 		tmp = ft_strjoin(*s, buff);
 		ft_strdel(s);
 		*s = ft_strdup(tmp);
 		ft_strdel(&tmp);
-	}
 	if (!buff[0] && !(*s)[0])
 		return (0);
 	return (1);
 }
 
-static int	get_len(char **s)
-{
-	int len;
-
-	len = 0;
-	while (((*s)[len] != '\n') && (*s)[len] != '\0')
-		len++;
-	return (len);
-}
-
 static int	set_str(char **s, char **line, char *tmp)
 {
-	int		len;
 	char	*tmp2;
 
 	if ((tmp = ft_strchr(*s, '\n')) == NULL && *s[0] != '\0')
@@ -58,7 +48,6 @@ static int	set_str(char **s, char **line, char *tmp)
 	}
 	else
 	{
-		len = get_len(s);
 		*tmp = '\0';
 		*line = ft_strdup(*s);
 		tmp2 = ft_strdup(tmp + 1);
