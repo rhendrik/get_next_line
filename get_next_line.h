aac95956542cc279cf25749eb42a4e450835f8dc
mistress_r <rhendrik@student.wethinkcode.co.za>
2019-08-19 10:02:24 +0200

    real last norm I hope

--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,17 @@
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhendrik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/19 10:00:53 by rhendrik          #+#    #+#             */
+/*   Updated: 2019/08/19 10:01:15 by rhendrik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef GET_NEXT_LINE_H
+#	define GET_NEXT_LINE_H
 
 #include "libft/libft.h"
 #include <stdio.h>
@@ -7,4 +19,4 @@
 
 int get_next_line(const int fd, char **line);
 
-#endif
+#	endif
